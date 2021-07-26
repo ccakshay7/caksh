796598961e5f67886e19fe25cd00e5a85aea5870
ccakshay7 <ccakshay7@gmail.com>
2021-07-26 11:12:58 +0530

    Added new DBms

---
+++ test3.sql
@@ -0,0 +1,9 @@
+BEGIN
+	For i in 1..2
+	loop
+		dbms_output.put_line(i);
+		dbms_output.put_line("Number"||i);
+	end loop;
+	dbms_output.put_line("Test");
+	dbmCs_output.put_line("Program test");
+END;
